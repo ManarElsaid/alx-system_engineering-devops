3c01e1f386f827439d77595901a0d144745a878e
Manar Elsaid <elsaidmanar@gmail.com>
2023-12-30 23:12:18 +0200

    4

--- 0x0C-web_server/4-not_found_page_404.sh
+++ 0x0C-web_server/4-not_found_page_404.sh
@@ -5,4 +5,6 @@ sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 sudo echo "Hello World!" | sudo tee /var/www/html/index.html
 sudo sed -i '/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/ permanent;' /etc/nginx/sites-enabled/default
+sudo echo "Ceci n'est pas une page" | sudo tee /var/www/html/404.html
+sudo sed -i '/server_name _;/a error_page 404 /404.html;\n\tlocation = /404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
