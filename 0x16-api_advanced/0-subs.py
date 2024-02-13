b9d34e8dd7e6c80f6c27bab63609b979682fbd1f
ManarElsaid <elsaidmanar@gmail.com>
2024-02-14 01:32:45 +0200

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
     
     try:
         base_url = f'https://www.reddit.com/r/{subreddit}/about.json'
-        response = requests.get(base_url, headers={'useragent' : 'Google Chrome Version 81.0.4044.129'})
+        response = requests.get(base_url, headers={'User-agent' : 'Google Chrome Version 81.0.4044.129'})
         return response.json().get('data').get('subscribers')
     except Exception:
         return 0
