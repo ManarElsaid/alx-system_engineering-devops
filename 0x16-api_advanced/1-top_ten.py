934c924893830746e00dae5bc7f7104b62054e50
ManarElsaid <elsaidmanar@gmail.com>
2024-02-14 02:07:39 +0200

    1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""  a function that queries the Reddit API and prints
+the titles of the first 10 hot posts listed for a given subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    a function that queries the Reddit API and prints
+    the titles of the first 10 hot posts listed for a given subreddit
+    """
+    if subreddit is None:
+        print("None")
+    
+    try:
+        url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+        response = requests.get(url, headers={'User-agent' : 'Google Chrome Version 81.0.4044.129'}, params={'limits' : 10})
+        my_data = response.json().get('data').get('children')
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
